6c5d43c5d8e39fc7f780f212b41ddfc2fbec95cf
Jamil Zazu <jamillzazu@gmail.com>
2021-06-16 19:24:28 -0400

    feat: Acessando propriedades no Styled Components

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -8,20 +8,36 @@ import {
   Amount,
   LastTransaction,
 } from './styles';
+interface HighlightCardProps {
+  type: 'up' | 'down' | 'total';
+  title: string;
+  amount: string;
+  lastTransaction: string;
+}
 
-function HighlightCard() {
+const icon = {
+  up: 'arrow-up-circle',
+  down: 'arrow-down-circle',
+  total: 'dollar-sign',
+};
+
+export function HighlightCard({
+  type,
+  title,
+  amount,
+  lastTransaction,
+}: HighlightCardProps) {
   return (
-    <Container>
+    <Container type={type}>
       <Header>
-        <Title>Entradas</Title>
-        <Icon name="arrow-up-circle" />
+        <Title type={type}>{title}</Title>
+        <Icon name={icon[type]} type={type} />
       </Header>
+
       <Footer>
-        <Amount>R$ 17.400,00</Amount>
-        <LastTransaction>Última entrada dia 13 de abril</LastTransaction>
+        <Amount type={type}>{amount}</Amount>
+        <LastTransaction type={type}>{lastTransaction}</LastTransaction>
       </Footer>
     </Container>
   );
 }
-
-export default HighlightCard;
